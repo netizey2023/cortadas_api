96fd56f61a98489cab05930682d8a9a391d0e742
netizey2023 <netizey2023@gmail.com>
2024-10-12 20:54:22 -0300

    Create captionsRoute.js

---
+++ routes/captionsRoute.js
@@ -0,0 +1,9 @@
+// routes/captionsRoute.js
+const express = require('express');
+const router = express.Router();
+const captionsController = require('../controller/captionsController');
+
+// Definir rota POST para pegar legendas
+router.post('/', captionsController.getCaptions);
+
+module.exports = router;
